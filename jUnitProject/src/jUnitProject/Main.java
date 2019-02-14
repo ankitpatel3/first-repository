2fa556b4657ee4e2698a7f7fc41fcd8755493dae
ankitpatel3 <ankitpatel3@USLMAANKITPATE1.us.deloitte.com>
2019-02-14 12:58:24 -0500

    Main class contains Calucator2 class.

--- jUnitProject/src/jUnitProject/Main.java
+++ jUnitProject/src/jUnitProject/Main.java
@@ -5,12 +5,35 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Check the numbers below:");
 		
-		Calculator calc = new Calculator();
-		System.out.println(calc.add(15, 20));
-		System.out.println(calc.subtract(15, 20));
-		System.out.println(calc.multiply(15, 20));
-		System.out.println(calc.devide(20, 3));
+		Calculator2 calc = new Calculator2();
+		System.out.println(calc.add(30, 20));
+		System.out.println(calc.subtract(10, 20));
+		System.out.println(calc.multiply(16, 20));
+		System.out.println(calc.devide(52, 3));
 
+	}	
+}
+
+class Calculator2 {
+	
+	public int add(int a, int b) {
+		int r = a + b;
+		return r;
+	}
+	
+	public int subtract(int a, int b) {
+		int r = a - b;
+		return r;
+	}
+	
+	public double devide(double a, double b) {
+		double r = a / b;
+		return r;
+	}
+	
+	public int multiply(int a, int b) {
+		int r = a * b;
+		return r;
 	}
 
 }
